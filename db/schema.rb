9a5126099ed703fcf61b518c2215d5396c9cb3dc
aubreeabril <aubree.abril@gmail.com>
2018-09-10 16:26:50 -0400

    added days model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_10_152053) do
+ActiveRecord::Schema.define(version: 2018_09_10_192540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "days", force: :cascade do |t|
+    t.integer "user_habit_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "dates", default: [], array: true
+  end
 
   create_table "habits", force: :cascade do |t|
     t.string "title"
